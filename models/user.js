a4e589d972f3032054cad0193b03852eeaf2e2b3
ahmmuh <mukhtar1100@hotmail.com>
2025-04-13 19:00:49 +0200

    changed in user schema

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export const userSchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     name: String,
     phone: String,
@@ -10,3 +10,5 @@ export const userSchema = new mongoose.Schema(
 );
 
 const User = mongoose.model("User", userSchema);
+
+export default User;
