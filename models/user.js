f388c1f89db0777d28333000a159326278f688ed
ahmmuh <mukhtar1100@hotmail.com>
2025-08-11 00:33:57 +0200

    ok

--- models/user.js
+++ models/user.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
 
   role: {
     type: String,
-    enum: ["Chef", "Specialist"],
+    enum: ["Avdelningschef ", "Områdeschef", "Enhetschef", "Specialare"],
   },
 
   unit: {
